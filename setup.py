b07f4bc4c4b80fbac34c83939e4cf9d3334d95b6
Darren Gibbard <dalgibbard@gmail.com>
2015-08-26 11:17:59 +0100

    Removed SSL and uPnP, and the dependancies for a stripped down mentor version (for cygwin and OSX)

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@
 from distutils.core import setup
 
 setup(name='mentor',
-    version=0.1,
-    description='Simple file sharing tool over HTTP/HTTPS',
+    version="0.1",
+    description='Simple file sharing tool over HTTP',
     author='fim',
     install_requires=[
-        'M2Crypto',
-        'miniupnpc',
         'gevent'],
 #    author_email='',
     scripts=['mentor']
